a065f372bc7f74fed3b18cc6ab049b1be3476a61
Keshav Sharma <sharmakraj93@gmail.com>
2020-10-31 02:15:05 +0530

    ES Lint Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,41 @@
-import { Component } from '@angular/core';
-import { fromWorker } from 'observable-webworker';
-import { Observable, of } from 'rxjs';
+import {Component} from '@angular/core';
+import {fromWorker} from 'observable-webworker';
+import {Observable, of} from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+/**
+ * Adds two numbers together.
+ * @param {int} num1 The first number.
+ * @param {int} num2 The second number.
+ * @return {int} The sum of the two numbers.
+ */
 export class AppComponent {
   title = 'prime-numbers';
 
+  /**
+ * Adds two numbers together.
+ * @param {int} num1 The first number.
+ * @param {int} num2 The second number.
+ */
   runWorker() {
-
-    const input$: Observable<any> = of({data: [1,2,3,4], newData:[]});
-
-    fromWorker<string, string>(() => new Worker('./demo.worker', { type: 'module'}), input$)
-      .subscribe(message => {
-        console.log(`Got message`, message);
-      });
-
+    const input$: Observable<any> = of({data: [1, 2, 3, 4], newData: []});
+    fromWorker<string, string>(() => new Worker('./demo.worker',
+      {type: 'module'}), input$)
+        .subscribe((message) => {
+          console.log(`Got message`, message);
+        });
   }
-  
 }
 
 if (typeof Worker !== 'undefined') {
   // Create a new
-  const worker = new Worker('./app.worker', { type: 'module' });
-  worker.onmessage = ({ data }) => {
+  const worker = new Worker('./app.worker', {type: 'module'});
+  worker.onmessage = ({data}) => {
     console.log(`page got message: ${data}`);
   };
   worker.postMessage('hello');
--- src/app/demo.worker.ts
+++ src/app/demo.worker.ts
@@ -1,18 +1,23 @@
-import { DoWork, ObservableWorker } from 'observable-webworker';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {DoWork, ObservableWorker} from 'observable-webworker';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @ObservableWorker()
+/**
+ * Adds two numbers together.
+ */
 export class DemoWorker implements DoWork<string, string> {
-
+  /**
+ * Adds two numbers together.
+ * @param {Observable} input$ The second number.
+ * @return {int} The sum of the two numbers.
+ */
   public work(input$: Observable<any>): Observable<any> {
-
     return input$.pipe(
-      map(data => {
-        data.newData = data.data;
-        return data;
-      })
+        map((data) => {
+          data.newData = data.data;
+          return data;
+        }),
     );
   }
-
 }
